d8401b65aa7cf525e623cc981fce558751c49e2c
Glushkow <73849882+Glushkow@users.noreply.github.com>
2021-04-24 19:04:56 +0300

    AC, DEL - function added

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -3,7 +3,9 @@ import {Button, Container, Current, Previous, Screen} from './Styled'
 
 export default function Calculator() {
 
+    const [previous, setPrevious] = useState('')
     const [current, setCurrent] = useState('')
+    const [operation, setOperation] = useState('')
 
     const appendValue = (el) => {
         const value = el.target.getAttribute('data')
@@ -13,14 +15,25 @@ export default function Calculator() {
 
     }
 
+    const handleDelete = () => {
+        setCurrent(String(current.slice(0, -1)))
+
+    }
+
+    const handleAllClear = () => {
+        setCurrent('')
+        setPrevious('')
+        setOperation('')
+    }
+
     return(
        <Container>
            <Screen>
-               <Previous></Previous>
+               <Previous>{previous} {operation}</Previous>
                <Current>{current}</Current>
            </Screen>
-           <Button gridSpan={2} control>AC</Button>
-           <Button control>DEL</Button>
+           <Button onClick={handleAllClear} gridSpan={2} control>AC</Button>
+           <Button onClick={handleDelete} control>DEL</Button>
            <Button operation>÷</Button>
            <Button data={'7'} onClick={appendValue}>7</Button>
            <Button data={'8'} onClick={appendValue}>8</Button>
